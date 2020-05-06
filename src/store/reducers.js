43a7e865c30dda21c4b45e20bf65266fe3fa1104
Navid <nkhalili@live.com>
2020-05-06 18:45:30 -0400

    Refactor reducers

--- src/store/reducers.js
+++ src/store/reducers.js
@@ -1,20 +1,11 @@
 import C from '../constants'
 
-export const goal = (state=10, action) => {
+export const goal = (state=10, action) => 
+  (action.type === C.SET_GOAL) ?
+    parseInt(action.payload) : 
+    state
 
-  if(action.type === C.SET_GOAL) {
-    return parseInt(action.payload)
-  } else {
-    return state
-  }
-
-}
-
-export const skiDay = (state=null, action) => {
-
-  if(action.type === C.ADD_DAY) {
-    return action.payload
-  } else {
-    return state
-  }
-}
+export const skiDay = (state=null, action) =>
+  (action.type === C.ADD_DAY) ?
+   action.payload :
+   state
