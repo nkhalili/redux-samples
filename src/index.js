ccb6e7562eeb8e142132ec48e2d96eb03b61f535
Navid <nkhalili@live.com>
2020-05-06 13:01:13 -0400

    First babel run!

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import C from './constants'
+import { allSkiDays, goal } from './initialState.json'
+
+console.log(`
+  Ski Day Counter
+  ===========================
+  This goal is ${goal} days
+  Initially there are ${allSkiDays.length} ski days in state
+
+  Constants (actions):
+  ---------------------------
+  ${Object.keys(C).join('\n     ')}
+`)
