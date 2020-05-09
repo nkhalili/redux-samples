1a4408d4642262c90e5c6ce6228761932a4e6db4
Navid <nkhalili@live.com>
2020-05-09 12:03:46 -0400

    Create middleware

---
+++ src/store/index.js
@@ -0,0 +1,45 @@
+import C from '../constants'
+import appReducer from './reducers'
+import { createStore, applyMiddleware } from "redux";
+
+// const consoleMessages = function(store) {
+//   return function(next){
+//     return function(action){
+
+//     }
+//   }
+// }
+
+// Short form of consoleMessages middleware
+const consoleMessages = store => next => action => {
+  
+  let result
+  
+  console.groupCollapsed(`dispatching action => ${action.type}`)
+  console.log('ski days', store.getState().allSkiDays.length)
+
+  // Call the next dispatch method in the middleware chain.
+  result = next(action)
+
+  let {allSkiDays, goal, errors, resortNames } = store.getState()
+
+  console.log(`
+
+      ski days: ${allSkiDays.length}
+      goal: ${goal}
+      fetching: ${resortNames.fetching}
+      suggestions: ${resortNames.suggestions}
+      errors: ${errors.length}
+      
+  `)
+  console.groupEnd()
+
+  // This will likely be the action itself, unless
+  // a middleware further in chain changed it.
+  return result
+
+}
+
+export default (initialState={}) => {
+  return applyMiddleware(consoleMessages)(createStore)(appReducer, initialState)
+}
